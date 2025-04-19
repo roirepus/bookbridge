340f5202b5d5e28405066802de54f40de4e08660
roirepus <rajsinsinwar159@gmail.com>
2025-04-19 21:09:03 +0530

    home page ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+
+export default function Home() {
+  return (
+    <>
+      <BookOverview
+        {...sampleBooks[0]}
+      />
+
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassname="mt-28"
+      />
+    </>
+  );
+}
