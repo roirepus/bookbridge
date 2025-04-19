45a8ea4e7964ea26d42a677150547f437478f42c
roirepus <rajsinsinwar159@gmail.com>
2025-04-19 18:09:22 +0530

    project init

---
+++ app/layout.tsx
@@ -0,0 +1,61 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import localFont from "next/font/local"
+import { ReactNode } from "react";
+
+const ibmPlexSans = localFont({
+  src: [
+    {
+      path: "./fonts/IBMPlexSans-Regular.ttf",
+      weight: "400",
+      style: "normal"
+    },
+    {
+      path: "./fonts/IBMPlexSans-Medium.ttf",
+      weight: "500",
+      style: "normal"
+    },
+    {
+      path: "./fonts/IBMPlexSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal"
+    },
+    {
+      path: "./fonts/IBMPlexSans-Bold.ttf",
+      weight: "700",
+      style: "normal"
+    },
+  ],
+});
+const bebasNeue = localFont({
+  src: [
+    {
+      path: "./fonts/BebasNeue-Regular.ttf",
+      weight: "400",
+      style: "normal"
+    },
+  ],
+  variable: "--bebas-neue"
+});
+
+
+export const metadata: Metadata = {
+  title: "BookBridge",
+  description: "BookBridge is a book borrowing library management solution",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
